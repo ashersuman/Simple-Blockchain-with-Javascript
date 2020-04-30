da31fbe84dd433e84a043b9253e7a352629402ec
ashersuman <user11root@gmail.com>
2020-05-01 01:04:34 +0530

    here comes miner

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previoushash='')
+    constructor(timestamp, transactions, previoushash='')
     {
-        this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previoushash = previoushash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -29,22 +36,58 @@ class Block{
 class BlockChain{
     constructor() {
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 3;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     //first block in the blockchain must be added manually and it is called Genesis block.
     createGenesisBlock(){
-        return new Block(0, "16/04/2020", "Genesis Block", "0");
+        return new Block("16/04/2020", "Genesis Block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
+    // <OLD MINING METHOD>
+    // addBlock(newBlock){
+    //     newBlock.previoushash = this.getLatestBlock().hash;
+    //     newBlock.mineBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+    
+    // <NEW MINING METHOD>
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
 
-    addBlock(newBlock){
-        newBlock.previoushash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+        console.log('Block successfully mined');
+        this.chain.push(block);
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalancedAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid(){
@@ -67,17 +110,15 @@ class BlockChain{
 
 let skCoin = new BlockChain();
 
-console.log('Mining Block 1...');
-skCoin.addBlock(new Block(1,"29/04/2020", {amount: 4}));
+skCoin.createTransaction(new Transaction('address1', 'address2', 100));
+skCoin.createTransaction(new Transaction('address2', 'address1', 50));
+//address1 , address2 are actually public key of wallet address
 
-console.log('Mining Block 2...');
-skCoin.addBlock(new Block(2,"30/04/2020", {amount: 10}));
+//console.log(skCoin.pendingTransactions);
+console.log("\nStarting the miner.....");
+skCoin.minePendingTransactions('xploit-address');
 
-// console.log('Is blockchain valid? ' + skCoin.isChainValid());
-
-// skCoin.chain[1].data = {amount: 1000};
-// skCoin.chain[1].hash = skCoin.chain[1].calculateHash();
-// console.log('Is blockchain valid? ' + skCoin.isChainValid());
-
-// console.log(JSON.stringify(skCoin, null, 4)); 
-
+//console.log(skCoin.pendingTransactions);
+console.log("\nStarting the miner again.....");
+skCoin.minePendingTransactions('kemon-address');
+console.log('\nBalance of Xploit is', skCoin.getBalancedAddress('xploit-address'));
