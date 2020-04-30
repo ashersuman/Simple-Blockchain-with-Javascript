b2bdb857935a89f9d3929473e053874c770a13b1
ashersuman <user11root@gmail.com>
2020-04-30 22:22:09 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, timestamp, data, previoushash='')
+    {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previoushash = previoushash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash()
+    {
+        return SHA256(this.index + this.previoushash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: "+ this.hash);
+    }
+}
+
+class BlockChain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
+    }
+
+    //first block in the blockchain must be added manually and it is called Genesis block.
+    createGenesisBlock(){
+        return new Block(0, "16/04/2020", "Genesis Block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previoushash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()){
+                return false;
+            }
+            
+            if(currentBlock.previoushash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let skCoin = new BlockChain();
+
+console.log('Mining Block 1...');
+skCoin.addBlock(new Block(1,"29/04/2020", {amount: 4}));
+
+console.log('Mining Block 2...');
+skCoin.addBlock(new Block(2,"30/04/2020", {amount: 10}));
+
+// console.log('Is blockchain valid? ' + skCoin.isChainValid());
+
+// skCoin.chain[1].data = {amount: 1000};
+// skCoin.chain[1].hash = skCoin.chain[1].calculateHash();
+// console.log('Is blockchain valid? ' + skCoin.isChainValid());
+
+// console.log(JSON.stringify(skCoin, null, 4)); 
+
